76bf97550e27d44525d424ba5e71d0fc86bd5d44
Konstantin Zhukov <kvzhkv@gmail.com>
2024-04-18 21:01:08 +0200

    add shortcodes & css filter to 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,22 @@
+const { readFileSync } = require("fs");
+const CleanCSS = require("clean-css");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addWatchTarget("./src/styles/");
     eleventyConfig.addPassthroughCopy({
         "src/assets": "/",
     });
+    eleventyConfig.addShortcode("inlineSvg", iconName =>
+        readFileSync(`./src/icons/${iconName}.svg`).toString(),
+    );
+    eleventyConfig.addPairedShortcode(
+        "block",
+        (children, className) => `<div class="${className}">${children}</div>`,
+    );
+    eleventyConfig.addFilter(
+        "cssmin",
+        code => new CleanCSS({}).minify(code).styles,
+    );
     return {
         dir: {
             input: "src",
